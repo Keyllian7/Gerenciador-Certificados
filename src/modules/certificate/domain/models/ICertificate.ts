a011fd6e10ea54b2c007f3e7f79892fff4265b55
Keyllian7 <keyllianazevedo2@gmail.com>
2025-01-13 14:45:12 -0300

    Criando tabela do certificado e sua interface

---
+++ src/modules/certificate/domain/models/ICertificate.ts
@@ -0,0 +1,12 @@
+export interface ICertificate {
+    student: string,
+    course: string,
+    hours: number,
+    instructor: string,
+    identification: string,
+    date: Date,
+    city: string,
+    createdAt: Date,
+    updatedAt: Date,
+    deletedAt?: null | Date,
+}
---
+++ src/modules/certificate/infra/typeorm/entities/Certificate.ts
@@ -0,0 +1,37 @@
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity("certificate")
+export class Certificate {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "varchar"})
+  student: string;
+
+  @Column({ type: "varchar"})
+  course: string;
+
+  @Column({ type: "int"})
+  hours: number;
+
+  @Column({ type: "varchar"})
+  instructor: string;
+
+  @Column({ type: "varchar"})
+  identification: string;
+
+  @Column({ type: "date"})
+  date: Date;
+
+  @Column({ type: "varchar"})
+  city: string
+
+  @CreateDateColumn({ name: "createdAt" })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: "updatedAt" })
+  updatedAt: Date;
+
+  @DeleteDateColumn({ name: "deletedAt" })
+  deletedAt: null | Date;
+}
