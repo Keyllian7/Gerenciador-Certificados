6f789d2e8484372f52974ff547ee73c979dd415e
Keyllian7 <keyllianazevedo2@gmail.com>
2025-01-13 17:02:29 -0300

    Criando migrações da tabela certificate

--- ormconfig.js
+++ ormconfig.js
@@ -5,11 +5,11 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    synchronize: true,
+    synchronize: false,
     logging: false,
     entities: ['src/modules/**/infra/typeorm/entities/*.ts'],
     migrations: ['src/shared/infra/typeorm/migrations/*.ts'],
-    command: {
+    cli: {
         migrations: 'src/shared/infra/typeorm/migrations',
     },
 };
