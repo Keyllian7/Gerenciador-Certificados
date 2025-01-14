3fc074e6e1011879465c0ded36af0fdd08c8fff2
Keyllian7 <keyllianazevedo2@gmail.com>
2025-01-14 19:31:02 -0300

    Correção no retorno do controller do certificate

--- src/modules/certificate/infra/http/controllers/CertificateController.ts
+++ src/modules/certificate/infra/http/controllers/CertificateController.ts
@@ -1,12 +1,16 @@
 import CreateCertificateService from "@modules/certificate/service/CreateCertificate";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import { container } from "tsyringe";
 
 export default class CertificateController {
-  public async create(req: Request, res: Response): Promise<Response> {
+  public async create(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
     const { student, course, hours, instructor, date, city } = req.body;
     const createCertificate = container.resolve(CreateCertificateService);
     const certificate = await createCertificate.execute({ student, course, hours, instructor, date, city });
-    return res.json(certificate);
+    res.json(certificate);
+    } catch (error) {
+      next(error)
+    }
   }
 }
