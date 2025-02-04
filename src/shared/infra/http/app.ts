fc0a8a763127b47dece9d0f4a9d7081352f43463
Keyllian7 <keyllianazevedo2@gmail.com>
2025-02-04 17:52:17 -0300

    remove: deleting test route for template

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -5,32 +5,8 @@ import router from './routes/app.routes';
 import './database/index';
 import '@shared/container'
 
-const path = require('path');
-const ejs = require('ejs');
 const app = express();
 const port = 3333;
-
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, '../../../../views'))
-app.use(express.static(path.join(__dirname, '../../../../views')))
-
-const dataCertificate = {
-  name: 'Keyllian Azevedo Quirino',
-  course: 'Node Course',
-  date: 'January 12, 2025',
-  instructor: 'Max Milyano Quirino',
-  hours: 40,
-  identification: '18236214',
-};
-
-app.get('/template', (req, res) => {
-  ejs.renderFile(path.join(__dirname,"../../../../views/index.ejs"), dataCertificate, (error: Error, certificate: string) => {
-    if (error) {
-      return res.json({message: 'Error reading file'});
-    }
-    return res.send(certificate);
-  })
-})
 
 app.use(express.json());
 app.use(router);
